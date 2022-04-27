efdf4d27405e67df70475f02bc455e37d85f8344
Derek Todd <2719352t@student.gla.ac.uk>
2022-04-27 11:54:08 +0100

    Tutorial sheet stuff

---
+++ tutorian_b3_code/server.py
@@ -0,0 +1,10 @@
+import socket
+import sys
+srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srv_sock.bind(("", int(sys.argv[1])))
+srv_sock.listen(5)
+while True:
+    cli_sock, cli_addr = srv_sock.accept()
+    request = cli_sock.recv(1024)
+    print(str(cli_addr) + ": " + request.decode('utf-8'))
+    cli_sock.close()
